97dbddda5398a66b00820a1d664f6a25cf406c44
Dimmension <87306693+Dimmension@users.noreply.github.com>
2023-04-02 18:13:22 +0300

    Update 5.py

--- HW3/5.py
+++ HW3/5.py
@@ -1,5 +1,7 @@
 array = list(map(int, input().split()))
 count = 0
+#решение за квадрат(можно и за линию)
+#запускаем от каждого числа проверку
 for i in range(len(array)):
     for j in range(i+1, len(array)):
         if array[i] == array[j]:
