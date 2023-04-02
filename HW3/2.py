d827b24bd07581801c0192240b987064704d0ee9
Dimmension <87306693+Dimmension@users.noreply.github.com>
2023-04-02 18:05:06 +0300

    Update 2.py

--- HW3/2.py
+++ HW3/2.py
@@ -1,21 +1,24 @@
 
-def addToSeq(s):
-    new = ''
+def addToSequence(s):
+    #создаем новую строку на основе старой и возвращаем её
+    new_segment = ''
     for i in range(len(s)):
         if s[i] == '0':
-            new += '1'
+            new_segment += '1'
         else:
-            new += '0'
-    return new
+            new_segment += '0'
+    return new_segment
 
 
-def morisTueSeq(n):
+def morisTueSequence(n):
+    #всегда с 0
     s = "0"
+    #n раз выполним итерацию добавления
     for i in range(1, n):
-        s += addToSeq(s)
+        s += addToSequence(s)
     return s
 
 
 n = int(input())
-
-print(morisTueSeq(n)[0:n])
+#если послед. > n нужен срез
+print(morisTueSequence(n)[0:n])
