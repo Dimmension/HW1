af32ee18be15af46e244e97e4e4020454815ef02
Dimmension <87306693+Dimmension@users.noreply.github.com>
2023-06-02 10:47:14 +0300

    Update 1.c

--- HW6_c/1.c
+++ HW6_c/1.c
@@ -4,7 +4,7 @@
 int main()
 {
     int a, b, c, d;
-    scanf("%d %d %d %d", &a, &a, &b, &c, &d)
+    scanf("%d %d %d %d", &a, &b, &c, &d)
     int i = c + (a - c + d - 1) / d * d;
     for (int i; i < b + 1; i += d) {
         printf("%d ", i);
